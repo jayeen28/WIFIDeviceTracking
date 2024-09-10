c80b2092c76fd74cc7905cb7cfcfed7831875379
jayeen28 <jayeen6783@gmail.com>
2024-09-10 11:09:33 +0600

    telegram part done

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const dgram = require('dgram');
 const replaceMacWithName = require('./lib/replaceMacWithName');
 const server = dgram.createSocket('udp4');
-
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.TG_BOT_TOKEN, { polling: true });
+const users = process.env.ALLOWED_TG_USERS.split(',').map(Number);
 
 server.on('error', (err) => {
     console.error(`server error:\n${err.stack}`);
@@ -15,7 +17,7 @@ server.on('message', (log = new Buffer("")) => {
         let [, msg] = log.split("wlan0: ") || [];
         if (msg) {
             msg = replaceMacWithName(msg);
-            console.log(msg);
+            users.forEach(user => bot.sendMessage(user, msg))
         }
     }
 });
